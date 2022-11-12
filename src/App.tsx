df6727b4f163e3d2093e51ad99607307b0efa5a6
Filipe Lopes <filipee.lopes92@gmail.com>
2022-11-12 16:12:34 -0300

    criando a parte esquerda do projeto onde contem o formulário para captuar as informações

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+import { useState } from 'react';
+import styles from './App.module.css';
+import powered from './assets/powered.png';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [checked, setChecked] = useState(false);
+
+  const handleChecked = (e : React.MouseEvent<HTMLElement>)=>{
+    if(e.currentTarget.getAttribute('id') === 'genero_feminino'){
+      setChecked(true)
+    }else{
+      setChecked(false)
+    }
+    
+  }
+
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> Filipe
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className={styles.main}>
+      <header className={styles.header}>
+        <div>
+          <img src={powered} alt="" width={50} />
+        </div>
+      </header>
+        <div className={styles.container}>
+          <div className={styles.leftSide}>
+            <h1>Calcule seu BF %</h1>
+            <p>
+              O método aqui utilizado para calcular o BF (“body fat” ou percentual de gordura corporal em português) 
+              foi desenvolvido pela Marinha dos EUA para medir a gordura corporal de seus integrantes, 
+              ou seja, é um método usado na prática e bastante difundido. Ele é baseado apenas em medidas corporais, portanto, 
+              fácil de se fazer em casa.
+
+            </p>
+            <form >
+              
+              <div className={styles.genderInput}>
+                <label htmlFor="genero"></label>
+                <input type='radio' onClick={handleChecked} id='genero_masculino' value='Homem' name='genero'/>Homem
+                <input type='radio' onClick={handleChecked}  id='genero_feminino' value='Mulher' name='genero'/>Mulher
+              </div>
+
+              <input 
+                type="number"
+                placeholder='Digite sua idade.'
+              />
+              <input 
+                type="number"
+                placeholder='Digite sua altura em (cm) Ex. 180.'
+              />
+              <input 
+                type="number"
+                placeholder='Digite a cicunferência do pescoço em (cm) Ex. 48.'
+              />
+             <input 
+                type="number"
+                placeholder='Digite a circunferência da cintura em (cm) Ex. 75.'
+             />
+             {checked &&
+              
+              <input 
+                type="number"
+                placeholder='Digite a circunferencia do quadril em (cm) Ex. 80.'
+              />
+              
+              }
+
+              <button className={styles.buttonCalc}>Calcular</button>
+            </form>
+          </div>
+          <div className={styles.rightSide}>...</div>
+        </div>
     </div>
   )
 }
